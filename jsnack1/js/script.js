16c28577f9ee2dd3e017faf4936f0df3d5d090fb
zhLisha <lisazhou1709@gmail.com>
2022-06-10 13:06:01 +0200

    first commit

---
+++ jsnack1/js/script.js
@@ -0,0 +1,27 @@
+/* 
+- Chiedere all'utente dei numeri da inesrire nell'array
+- Continuare finche' la somma di tutti i numeri e' inferiore a 50.
+*/
+
+// Creazione Array numeri utente
+const userNumbersArray = []; 
+
+let sum = 0; 
+
+while(sum < 50){
+
+    // Chiedere il numero 
+    const userNumber = parseInt( prompt('Dammi un numero inferiore a 50') );
+
+    // Addizione dei numeri 
+    sum += userNumber;
+
+    console.log(sum);
+
+    // Inserimento numero in Array
+    if(sum <= 50){
+        userNumbersArray.push(userNumber);
+    }
+}
+
+console.log(userNumbersArray);
---
+++ jsnack2/js/script.js
@@ -0,0 +1,16 @@
+// Calcola la somma e la media dei primi 10 numeri 
+
+let sum = 0;
+//Generatore numeri 
+for( let i = 1; i <= 10; i++ ){
+    // Singoli numeri da 0 a 10
+    const singleNumber = i;
+
+    // Somma dei numeri da 0 a 10
+    sum += singleNumber;
+}
+
+// Media dei numeri 
+const finalResult = sum / 10;
+
+
